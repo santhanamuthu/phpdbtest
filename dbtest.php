856a87712adcc44bd02160430d1aa28b966d3761
santhanamuthu <33146563+santhanamuthu@users.noreply.github.com>
2021-06-23 22:45:56 +0530

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -7,18 +7,12 @@ $dbpwd = "sampledb";;
  
 
 
-$connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
-
-
-$query = "SELECT * from users" or die("Error in the consult.." . mysqli_error($connection));
-
-echo "Hello All.. Here is the list of users: <br>";
-$rs = $connection->query($query);
-while ($row = mysqli_fetch_assoc($rs)) {
-    echo "User Id: ".$row['user_id'] . " User Name: " . $row['username'] . "<br>";
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+if ($connection->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+} else {
+    printf("Connected to the database");
 }
-echo "End of the list <br>";
-
-mysqli_close($connection);
-
+$connection->close();
 ?>
